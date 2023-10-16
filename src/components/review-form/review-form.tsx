bd10ac3f8b2f3f63b07cb37366315cf57c7f18d0
art3xa <artem.romanov.03@bk.ru>
2023-10-16 07:17:23 +0500

    Add save reviewForm to state

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -1,14 +1,53 @@
+import {ChangeEvent, FormEvent, useRef, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {films} from '@mocks/films.ts';
+import type {Review} from 'types/review.ts';
+
 export function ReviewForm() {
+
+  const [filmRating, setFilmRating] = useState(0);
+  const commentRef = useRef<HTMLTextAreaElement>(null);
+  const navigate = useNavigate();
+  const [reviewForm, setReviewForm] = useState<Review>({
+    id: 0,
+    text: '',
+    author: 'somebody',
+    date: '2023-10-10',
+    rating: 0,
+    filmId: 0,
+  });
+  const film = films[0];
+  const onSubmit = (rating: number, comment: string) => {
+    const review: Review = {
+      id: film.id,
+      text: comment,
+      author: 'somebody',
+      date: '2023-10-10',
+      rating: rating,
+      filmId: film.id,
+    };
+    setReviewForm(review);
+    navigate(`films/${film.id}`);
+  };
+  const handleOnSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    if (filmRating && commentRef.current?.value) {
+      onSubmit(filmRating, commentRef.current.value);
+    }
+  };
+  const handleOnChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setFilmRating(Number(evt.target.value));
+  };
   return (
     <div className="add-review">
-      <form action="#" className="add-review__form">
+      <form action="#" className="add-review__form" onSubmit={handleOnSubmit}>
         <div className="rating">
           <div className="rating__stars">
             {Array.from({ length: 10 }, (_, i) => i + 1)
               .reverse()
               .map((number) => (
                 <>
-                  <input key={`star-${number}`} className="rating__input" id={`star-${number}`} type="radio" name="rating" value={`${number}`}/>
+                  <input key={`star-${number}`} onChange={handleOnChange} checked={reviewForm.rating === number} className="rating__input" id={`star-${number}`} type="radio" name="rating" value={`${number}`}/>
                   <label className="rating__label" htmlFor={`star-${number}`}>Rating ${number}</label>
                 </>
               ))}
@@ -16,7 +55,7 @@ export function ReviewForm() {
         </div>
 
         <div className="add-review__text">
-          <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+          <textarea ref={commentRef} className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
           <div className="add-review__submit">
             <button className="add-review__btn" type="submit">Post</button>
           </div>

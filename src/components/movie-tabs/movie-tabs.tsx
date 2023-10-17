ee3e6356dd945cbda5b89986480c21802aee7d3a
art3xa <artem.romanov.03@bk.ru>
2023-10-17 15:58:36 +0500

    Add tabs

--- src/components/movie-tabs/movie-tabs.tsx
+++ src/components/movie-tabs/movie-tabs.tsx
@@ -5,6 +5,7 @@ import {Overview} from '@components/movie-tabs/overview/overview.tsx';
 import {useState} from 'react';
 import {reviews} from '@mocks/reviews.ts';
 import {Film} from 'types/film.ts';
+import {MovieTab} from '@components/movie-tabs/movie-tab.tsx';
 
 type MovieTabsProps = {
   films: Film[];
@@ -22,18 +23,19 @@ export function MovieTabs({films}: MovieTabsProps) {
         return <Overview films={films}/>;
     }
   };
-
   return (
     <div className="film-card__desc">
       <nav className="film-nav film-card__nav">
         <ul className="film-nav__list">
           {
             Object.values(MovieTabEnum).map((currentTab) =>
-              (
-                <li key={currentTab} className={`film-nav__item ${movieTab === currentTab ? ' film-nav__item--active' : ''}`}>
-                  <div onClick={() => setMovieTab(currentTab)} className="film-nav__link">{currentTab}</div>
-                </li>
-              ))
+              <MovieTab key={currentTab} name={currentTab} onClick={setMovieTab} isActive={movieTab === currentTab}/>
+              // (
+              //   <li key={currentTab} className={`film-nav__item ${movieTab === currentTab ? ' film-nav__item--active' : ''}`}>
+              //     <div onClick={() => setMovieTab(currentTab)} className="film-nav__link">{currentTab}</div>
+              //   </li>
+              // )
+            )
           }
         </ul>
       </nav>

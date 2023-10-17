b61c7ee7e8cbcffcc5713b45f34b868f45902439
art3xa <artem.romanov.03@bk.ru>
2023-10-17 15:39:25 +0500

    Fix promoFilm

--- src/mocks/films.ts
+++ src/mocks/films.ts
@@ -25,6 +25,7 @@ export const promoFilm: Film = {
   runTime: '1h 39m',
   description: 'In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave\'s friend and protege. Gustave prides himself on providing first-class service to the hotel\'s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave\'s lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.',
   rating: 8.9,
+  previewLink: 'https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm',
 };
 
 export const films: Film[] = [

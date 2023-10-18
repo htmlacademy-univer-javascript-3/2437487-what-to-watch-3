cd67d3f0c7eb55376eb61f19ecfa32fa9b7f4308
art3xa <artem.romanov.03@bk.ru>
2023-10-19 02:15:32 +0500

    Add check creds

--- src/pages/sign-in-page/sign-in-page.tsx
+++ src/pages/sign-in-page/sign-in-page.tsx
@@ -1,16 +1,66 @@
 import {Header} from '@components/header/header.tsx';
 import {Footer} from '@components/footer/footer.tsx';
+import {useRef, useState} from 'react';
+import {useAppDispatch, useAppSelector} from '../../hooks';
+import {getAuthStatus} from 'store/reducer/user-reducer/action.ts';
+import {useNavigate} from 'react-router-dom';
+import {AuthStatus} from 'types/auth-status.ts';
+import {AppRoute} from 'types/app-route.ts';
+import {AuthData} from 'types/user.ts';
+import {loginAction} from 'store/api-action.ts';
+import {FormEvent} from 'react';
 
 export function SignInPage() {
-  const isInvalidEmail = false;
-  const isInvalidPassword = false;
-
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const authStatus = useAppSelector(getAuthStatus);
+  const navigate = useNavigate();
+  const [isInvalidEmail, setIsInvalidEmail] = useState(false);
+  const [isInvalidPassword, setIsInvalidPassword] = useState(false);
+  const [isError, setIsError] = useState(false);
+  if (authStatus === AuthStatus.Auth) {
+    navigate(AppRoute.Main);
+  }
+  const checkEmail = (email: string) => {
+    const reEmail = /\S+@\S+\.\S+/;
+    const result = reEmail.test(email);
+    setIsInvalidEmail(!result);
+    return result;
+  };
+  const checkPassword = (password: string) => {
+    const rePassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const result = rePassword.test(password);
+    setIsInvalidPassword(!result);
+    return result;
+  };
+  const dispatch = useAppDispatch();
+  const onSubmit = (authData: AuthData) =>
+    dispatch(loginAction(authData))
+      .then(() => {
+        if (authStatus === AuthStatus.Auth) {
+          navigate(AppRoute.Main);
+        }
+      })
+      .catch(() => setIsError(true));
+  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    if (emailRef.current !== null
+      && passwordRef.current !== null
+      && checkEmail(emailRef.current.value)
+      && checkPassword(passwordRef.current.value)) {
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
+      onSubmit({login: email, password});
+    } else {
+      setIsError(true);
+    }
+  };
   return (
     <div className="user-page">
       <Header isSignInPage isUserPage/>
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form action="#" className="sign-in__form" onSubmit={handleFormSubmit}>
           {
             isInvalidEmail &&
             <div className="sign-in__message">
@@ -23,13 +73,19 @@ export function SignInPage() {
               <p>Please enter a valid password</p>
             </div>
           }
+          {
+            isError &&
+            <div className="sign-in__message">
+              <p>We can’t recognize this email <br/> and password combination. Please try again.</p>
+            </div>
+          }
           <div className="sign-in__fields">
             <div className={`sign-in__field ${isInvalidEmail ? 'sign-in__field--error' : ''}`}>
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email"/>
+              <input className="sign-in__input" ref={emailRef} type="email" placeholder="Email address" name="user-email" id="user-email"/>
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className={`sign-in__field ${isInvalidPassword ? 'sign-in__field--error' : ''}`}>
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password"/>
+              <input className="sign-in__input" ref={passwordRef} type="password" placeholder="Password" name="user-password" id="user-password"/>
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>

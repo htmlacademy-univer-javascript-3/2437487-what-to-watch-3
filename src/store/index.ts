0980dd880b6ff997040f27e3508dd94c41718b32
art3xa <artem.romanov.03@bk.ru>
2023-10-18 09:25:55 +0500

    Add middleware api

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {reducer} from 'store/reducer/reducer.ts';
+import {createAPI} from 'services/api.ts';
 
+const api = createAPI();
 export const store = configureStore({
   reducer: reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    })
 });
